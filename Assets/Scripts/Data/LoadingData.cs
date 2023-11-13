6e85d6cf8858d5ad342090beabd113da07dde873
Shahadat Hossain Shamim <shamim131666@gmail.com>
2023-11-13 18:12:09 +0600

    With Loading Screen

---
+++ Assets/Scripts/Data/LoadingData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[CreateAssetMenu(menuName = "Studio-23/Loading Data")]
+public class LoadingData : ScriptableObject
+{
+    public Sprite LoadingSprite;
+    public Slider LoadingSlider;
+    public float LoadingTime;
+}
--- Assets/Scripts/LoadingScreenManager.cs
+++ Assets/Scripts/LoadingScreenManager.cs
@@ -1,18 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoadingScreenManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private LoadingData _loadingData;
+    [SerializeField] private GameObject _loaderUI;
+    private Slider _progressSlider;
+
+    private void Start()
     {
-        
+        Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Initialize()
     {
-        
+        if (_loadingData != null)
+        {
+            _progressSlider = _loaderUI.GetComponentInChildren<Slider>(); // Corrected assignment
+            _loaderUI.GetComponent<Image>().sprite = _loadingData.LoadingSprite;
+        }
+        else
+        {
+            Debug.LogError("No Loading Data Assigned");
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void EnableLoad()
+    {
+        LoadScene(1, _loadingData.LoadingTime);
+    }
+
+    private async void LoadScene(int index, float loadingTime)
+    {
+        await LoadSceneAsync(index, loadingTime);
+    }
+
+    private async UniTask LoadSceneAsync(int index, float loadingTime)
+    {
+        _loadingData.LoadingSlider.value = 0;
+        _loaderUI.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+        asyncOperation.allowSceneActivation = false;
+
+        float progress = 0;
+        float elapsedTime = 0;
+
+        while (!asyncOperation.isDone)
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime >= loadingTime)
+            {
+                progress = 1;
+                asyncOperation.allowSceneActivation = true;
+            }
+            else
+            {
+                progress = elapsedTime / loadingTime;
+            }
+
+            _progressSlider.value = progress;
+
+            await UniTask.Yield();
+        }
     }
 }
+
+
