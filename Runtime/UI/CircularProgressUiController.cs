ec559e89382c0baa104207440794f4d6906adb34
dipokDipu <90243333+dipokDipu@users.noreply.github.com>
2024-02-23 16:07:58 +0600

    Meta updated

---
+++ Runtime/UI/CircularProgressUiController.cs
@@ -0,0 +1,15 @@
+using Studio23.SS2.SceneLoadingSystem.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircularProgressUiController : AbstractLoadingScreenUI
+{
+
+    [Header("UI")]
+    [SerializeField] private Image _loadingImageSlot;
+
+    public override void UpdateProgress(float progress)
+    {
+        _loadingImageSlot.fillAmount = progress;
+    }
+}
---
+++ Runtime/UI/HorizontalProgressUIController.cs
@@ -0,0 +1,16 @@
+using Studio23.SS2.SceneLoadingSystem.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HorizontalProgressUIController : AbstractLoadingScreenUI
+{
+    [Header("UI")]
+    [SerializeField] private Image _loadingImageSlot;
+
+    public override void UpdateProgress(float progress)
+    {
+        _loadingImageSlot.fillAmount = progress;
+    }
+
+}
