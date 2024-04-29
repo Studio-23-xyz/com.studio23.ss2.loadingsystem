6a688b74fe4d4e3d138f41a16365f71ef880ffc0
dipokDipu <90243333+dipokDipu@users.noreply.github.com>
2024-04-29 17:27:26 +0600

    Fixed Dictonary Bugs

---
+++ Runtime/Data/HintData/LoadingScreenTextTable.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+using Studio23.SS2.SceneLoadingSystem.Extension;
+using UnityEngine;
+
+namespace Studio23.SS2.SceneLoadingSystem.Data
+{
+    [CreateAssetMenu(menuName = "Studio-23/LoadingSystem/Loading Screen TextTable")]
+    [System.Serializable]
+    public class LoadingScreenTextTable : ScriptableObject
+    {
+        public List<TextData> HintsInfo;
+
+        public TextData GetHint(TextType type)
+        {
+            HintsInfo = ShuffleListExtension.Shuffle(HintsInfo);
+            var hintData = HintsInfo.FindAll(x => x.Type == type);
+            return hintData[Random.Range(0,hintData.Count)];
+        }
+
+        public TextData GetHint()
+        {
+            HintsInfo = ShuffleListExtension.Shuffle(HintsInfo);
+            var hintData = HintsInfo.FirstOrDefault();
+            return hintData != null ? HintsInfo[0] : null;
+        }
+    }
+}
