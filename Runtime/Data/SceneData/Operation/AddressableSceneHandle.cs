f8ec82ea7e9a926f9ff535fa51caaa02b056e350
dipokDipu <90243333+dipokDipu@users.noreply.github.com>
2024-09-17 12:55:18 +0600

    Fixed loading scene not working properly

---
+++ Runtime/Data/SceneData/Operation/AddressableSceneHandle.cs
@@ -0,0 +1,26 @@
+
+using Cysharp.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+namespace Studio23.SS2.SceneLoadingSystem.Data
+{
+    public class AddressableSceneHandle
+    {
+        internal AsyncOperationHandle<SceneInstance> LoadHandle { get; }
+
+        internal AddressableSceneHandle(AddressableSceneData data, bool isSingleLoad, bool activateOnLoad)
+        {
+            LoadHandle = Addressables.LoadSceneAsync(data.Scene, isSingleLoad? LoadSceneMode.Single : LoadSceneMode.Additive, activateOnLoad);
+        }
+
+        internal async UniTask UnloadScene()
+        { 
+            var operation =  Addressables.UnloadSceneAsync(LoadHandle);
+            await UniTask.WaitUntil(() => operation.IsDone);
+        }
+    }
+}
+
